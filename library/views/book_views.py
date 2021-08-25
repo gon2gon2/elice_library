02ea0b6c68daecea781a38dc9fbde40e33259dfe
gon2gon2 <ark5354@gmail.com>
2021-08-25 16:52:16 +0900

    book rating

--- library/views/book_views.py
+++ library/views/book_views.py
@@ -12,8 +12,9 @@ def before_request():
 
 @bp.route('/mainpage', methods=["GET"])
 def mainpage():
-    books = Book.query.all()    
+    books = Book.query.all()
     return render_template('mainpage.html', books=books)
+
 
 @bp.route('/detail/<int:book_id>', methods=['GET', 'POST'])
 def detail(book_id):
@@ -22,13 +23,30 @@ def detail(book_id):
         return render_template('detail.html', book=book)
     
     elif request.method == 'POST':
+        # 댓글 추가 로직
         user_id = session['user_id']
         comment = request.form['comment']
+        rating = len(request.form.keys()) - 1
+
+        
         reply = Reply(
             user_id = user_id,
             book_id = book_id,
+            rating = rating,
             comment = comment
         )
         db.session.add(reply)
+        
+        # book의 rating 바꿔주는 로직
+        book = Book.query.filter(Book.id == book_id).first()
+        reply = book.reply
+        star = []
+        for r in reply:
+            print(r)
+            star.append(r.rating)
+        avg = sum(star)/len(star)
+        book.rating = avg
+        
+        
         db.session.commit()
         return redirect(f'/detail/{book_id}')
